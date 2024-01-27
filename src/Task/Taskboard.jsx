0154c12ebde8d9cc3087e045345bc553d5456b21
ahmmedrasel-dev <rahmmed.info@gmail.com>
2024-01-28 01:45:42 +0600

    task list add daynamic task data

--- src/Task/Taskboard.jsx
+++ src/Task/Taskboard.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 export default function Taskboard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "I want to learn React  such than i can teat it like my walve and make it do whatever i want to do",
+    tags: ["web", "next", "react"],
+    priority: "Hight",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -13,7 +25,7 @@ export default function Taskboard() {
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction />
 
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
