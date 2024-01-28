d8acf81b069007abf8592a94ae6f93a5927adff8
ahmmedrasel-dev <rahmmed.info@gmail.com>
2024-01-29 01:51:47 +0600

    delete all task

--- src/Task/Taskboard.jsx
+++ src/Task/Taskboard.jsx
@@ -47,6 +47,12 @@ export default function Taskboard() {
     setTasks(taskIdAfterDelete);
   }
 
+  // Delete All Task
+  function handleAllDeleteTask() {
+    tasks.length = 0;
+    setTasks([...tasks]);
+  }
+
   function handleCloseModal() {
     setShowAddModla(false);
     setTaskToUpdate(null);
@@ -66,7 +72,10 @@ export default function Taskboard() {
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction onAddClick={() => setShowAddModla(true)} />
+          <TaskAction
+            onAddClick={() => setShowAddModla(true)}
+            handleAllDeleteTask={handleAllDeleteTask}
+          />
 
           <TaskList
             tasks={tasks}
