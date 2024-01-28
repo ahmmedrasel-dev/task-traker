93b3ef30679928b650b572fa82dfabeba5717d4e
ahmmedrasel-dev <rahmmed.info@gmail.com>
2024-01-29 01:23:26 +0600

    task edit and update done

--- src/Task/TaskList.jsx
+++ src/Task/TaskList.jsx
@@ -1,5 +1,5 @@
 import { FaStar } from "react-icons/fa";
-export default function TaskList({ tasks, onAddClick }) {
+export default function TaskList({ tasks, handleEditTask }) {
   return (
     <div className="overflow-auto">
       <table className="table-fixed overflow-auto xl:w-full">
@@ -60,7 +60,12 @@ export default function TaskList({ tasks, onAddClick }) {
               <td>
                 <div className="flex items-center justify-center space-x-3">
                   <button className="text-red-500">Delete</button>
-                  <button className="text-blue-500">Edit</button>
+                  <button
+                    className="text-blue-500"
+                    onClick={() => handleEditTask(task)}
+                  >
+                    Edit
+                  </button>
                 </div>
               </td>
             </tr>
--- src/Task/Taskboard.jsx
+++ src/Task/Taskboard.jsx
@@ -10,19 +10,48 @@ export default function Taskboard() {
     description:
       "I want to learn React  such than i can teat it like my walve and make it do whatever i want to do",
     tags: ["web", "next", "react"],
-    priority: "Hight",
+    priority: "high",
     isFavorite: true,
   };
   const [tasks, setTasks] = useState([defaultTask]);
   const [showAddModal, setShowAddModla] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
 
-  function handleAddTask(newTask) {
-    setTasks([...tasks, newTask]);
+  function handleAddTask(newTask, isAdd) {
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          }
+          return task;
+        })
+      );
+    }
     setShowAddModla(false);
+    setTaskToUpdate(null);
+  }
+
+  function handleEditTask(task) {
+    setTaskToUpdate(task);
+    setShowAddModla(true);
+  }
+
+  function handleCloseModal() {
+    setShowAddModla(false);
+    setTaskToUpdate(null);
   }
   return (
     <section className="mb-20" id="tasks">
-      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
+      {showAddModal && (
+        <AddTaskModal
+          onSave={handleAddTask}
+          taskToUpdate={taskToUpdate}
+          handleCloseModal={handleCloseModal}
+        />
+      )}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
@@ -31,7 +60,7 @@ export default function Taskboard() {
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction onAddClick={() => setShowAddModla(true)} />
 
-          <TaskList tasks={tasks} />
+          <TaskList tasks={tasks} handleEditTask={handleEditTask} />
         </div>
       </div>
     </section>
